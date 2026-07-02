99410ef3926833922562c5dc856250bf16f0a5cd
Nutchanon547 <32757969+titleZa89@users.noreply.github.com>
2026-07-01 20:08:46 -0700

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -195,9 +195,9 @@ function HomePage() {
             </div>
             <div style={{ padding: '10px', display: 'flex', justifyContent: 'center', width: '100%' }}>
   <iframe 
-    src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpolice5.nan&tabs=timeline&width=500&height=700&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
+    src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpolice5.nan&tabs=timeline&width=500&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
     width="500" 
-    height="700" 
+    height="1500" 
     style={{ border: 'none', overflow: 'hidden', width: '100%', maxWidth: '500px' }} 
     allow="encrypted-media"
   ></iframe>
